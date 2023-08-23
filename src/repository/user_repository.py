5dd160a34af52a1e29a4affc1b6424b9e7a7e52d
blkd <blkd.html@gmail.com>
2023-08-23 10:29:58 -0300

    create connection of databse

--- src/repository/user_repository.py
+++ src/repository/user_repository.py
@@ -1,2 +1,22 @@
+import mysql.connector
+from mysql.connector import Error
+
 class UserRepository:
-    pass
+    def __init__(self):
+        self.enginer = self.__create_conection
+
+    def __create_conection():
+        try:
+            return mysql.connector.connect(
+                host="localhost",
+                user="root",
+                passwd="",
+                database="blog_infocell"
+            )
+        except Error as e:
+            return None
+    
+
+    
+user = UserRepository()
+print(user.enginer)
